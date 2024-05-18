48ea9986f78059f41856d0c6464b7adaf8e34d74
Anjeza Gashi <108656875+anjezagashi@users.noreply.github.com>
2024-05-19 00:33:45 +0200

    Send function completed

--- main.py
+++ main.py
@@ -53,3 +53,41 @@ def Send():
             f.write(file_hash + encrypted_data)
 
         return encrypted_filename
+    
+    def sender():
+        encrypted_filename = encrypt_file(filename, "public_keys/receiver_public_key.pem")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((socket.gethostname(), 8080))
+        s.listen(1)
+        print('waiting for incoming connections...')
+        conn, addr = s.accept()
+        print(f'Connection from {addr}')
+
+        with open(encrypted_filename, 'rb') as file:
+            file_data = file.read(1024)
+            while file_data:
+                conn.send(file_data)
+                file_data = file.read(1024)
+        conn.close()
+        print("Data has been transmitted successfully...")
+
+    def start_sending():
+        threading.Thread(target=sender).start()
+
+    image_icon1 = PhotoImage(file="Image/send.png")
+    window.iconphoto(False, image_icon1)
+
+    Sbackground = PhotoImage(file="Image/sender.png").subsample(1)
+    Label(window, image=Sbackground).place(x=-2, y=0)
+
+    Mbackground = PhotoImage(file="Image/id.png").subsample(2)
+    Label(window, image=Mbackground, bg='#f4fdfe').place(x=50, y=300)
+
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    Label(window, text=f'ID: {ip}', bg='white', fg='black').place(x=160, y=340)
+
+    Button(window, text="+ select file", width=10, height=1, font='arial 14 bold', bg="#fff", fg="#000", command=select_file).place(x=190, y=230)
+    Button(window, text="SEND", width=8, height=1, font='arial 14 bold', bg='#000', fg="#fff", command=start_sending).place(x=330, y=230)
+
+    window.mainloop()
