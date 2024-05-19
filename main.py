50f755c3d786bbec31ce9d4414e00e98b53dc96e
Anjeza Sfishta <anjeza.sfishta@student.uni-pr.edu>
2024-05-19 14:53:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -179,4 +179,23 @@ def Receive():
 
 image_icon = PhotoImage(file="Image/icon.png")
 root.iconphoto(False, image_icon)
+Label(root, text="File Transfer", font=('Acumin Variable Concept', 20, 'bold'), bg="#f4fdfe").place(x=20, y=30)
+
+Frame(root, width=1200, height=2, bg="#f3f5f6").place(x=25, y=80)
+
+send_image = PhotoImage(file="Image/send.png").subsample(3)
+send = Button(root, image=send_image, bg="#f4fdfe", bd=0, command=Send)
+send.place(x=50, y=100)
+
+receive_image = PhotoImage(file="Image/receive.png").subsample(3)
+receive = Button(root, image=receive_image, bg="#f4fdfe", bd=0, command=Receive)
+receive.place(x=300, y=100)
+
+Label(root, text="Send", font=('Acumin Variable Concept', 12, 'bold'), bg="#f4fdfe").place(x=65, y=200)
+Label(root, text="Receive", font=('Acumin Variable Concept', 12, 'bold'), bg="#f4fdfe").place(x=300, y=200)
+
+background = PhotoImage(file="Image/background.png")
+Label(root, image=background).place(x=-1, y=335)
+
+root.mainloop()
 
